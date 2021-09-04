3aeadf0a39b251b1d94b7d68fdb33e96d720ba39
SungaHwang <74399508+SungaHwang@users.noreply.github.com>
2021-09-04 22:39:17 +0900

    Update 상관분석.R

--- notebook/황성아/상관분석.R
+++ notebook/황성아/상관분석.R
@@ -1,5 +1,5 @@
 # 상관분석
-data <- read.table("C:/Users/user/Desktop/data.txt",header = T, sep="\t")
+data <- read.table("/data.txt",header = T, sep="\t")
 head(data)
 install.packages("psych")
 library(psych)
